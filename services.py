f308fbde87f0a6d75239c1fa2551e6d8936b221a
Caio Ximenes <xiumenes224@gmail.com>
2026-05-01 21:57:01 -0400

    feat: adiciona funcao depositar

--- services.py
+++ services.py
@@ -6,7 +6,19 @@ def saldo(models):
     return saldo
 
 
-
+def depositar(models, valor_deposito):
+    if valor_deposito > 0:
+        data_transacao = Datetime.now()
+        data_transacao = data_transacao.strftime("%Y/%m/%d %H:%M:%S")
+        
+        transacoes = {
+            "tipo":"deposito",
+            "valor":"valor_deposito",
+            "data_hora":data_transacao,
+            "mensagem":"f'Deposito de R${valor_deposito} as {data_transacao}",
+        }
+        models.historico.append(transacoes)
+        models.saldo += valor_deposito
 
 
 
